92dcc876602f59c101b09b2b3e44d253c513de16
Paris <paris@octr.dev>
2023-08-02 03:19:40 +0100

    Fixed background image scrolling

--- Assets/_Game/Scripts/InfiniteSkyboxScroll.cs
+++ Assets/_Game/Scripts/InfiniteSkyboxScroll.cs
@@ -4,27 +4,55 @@ public class InfiniteSkyboxScroll : MonoBehaviour
 {
     public float scrollSpeed = 1.0f;
 
-    private Material material;
-    private float offset;
+    private SpriteRenderer spriteRenderer;
+    private float spriteWidth;
+    private Transform cloneTransform;
+    private GameObject cloneObject;
+    private SpriteRenderer cloneSpriteRenderer;
 
     void Start()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        material = spriteRenderer.material;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = spriteRenderer.bounds.size.x;
+
+        // Create a clone sprite and position it to the right of the original sprite
+        cloneObject = new GameObject("CloneSprite");
+
+        cloneSpriteRenderer = cloneObject.AddComponent<SpriteRenderer>();
+        cloneSpriteRenderer.sprite = spriteRenderer.sprite;
+        cloneSpriteRenderer.flipX = true; // Ensure the clone is flipped horizontally
+
+        // Copy essential properties from the original sprite renderer to the clone
+        cloneSpriteRenderer.sortingLayerID = spriteRenderer.sortingLayerID;
+        cloneSpriteRenderer.sortingOrder = spriteRenderer.sortingOrder;
+        cloneSpriteRenderer.color = spriteRenderer.color;
+        cloneSpriteRenderer.sharedMaterial = spriteRenderer.sharedMaterial;
+
+        // Additional properties to copy
+        cloneSpriteRenderer.drawMode = spriteRenderer.drawMode;
+        cloneSpriteRenderer.size = spriteRenderer.size;
+        cloneSpriteRenderer.maskInteraction = spriteRenderer.maskInteraction;
+        cloneSpriteRenderer.spriteSortPoint = spriteRenderer.spriteSortPoint;
+
+        cloneTransform = cloneObject.transform;
+        cloneTransform.position = transform.position + new Vector3(spriteWidth, 0f, 0f);
     }
 
     void Update()
     {
-        // Move the skybox horizontally based on time and speed
-        offset += Time.deltaTime * scrollSpeed;
+        // Move the original and clone sprites horizontally based on time and speed
+        float offsetX = Time.deltaTime * scrollSpeed;
+        transform.position -= new Vector3(offsetX, 0f, 0f);
+        cloneTransform.position -= new Vector3(offsetX, 0f, 0f);
 
-        // Ensure the offset stays between 0 and 1
-        if (offset > 1.0f)
+        // Reset the positions of the original and clone sprites when they go off-screen
+        if (transform.position.x <= -spriteWidth)
         {
-            offset -= 1.0f;
+            transform.position += new Vector3(spriteWidth * 2, 0f, 0f);
         }
-
-        // Apply the offset to the material's main texture
-        material.mainTextureOffset = new Vector2(offset, 0);
+        if (cloneTransform.position.x <= -spriteWidth)
+        {
+            cloneTransform.position += new Vector3(spriteWidth * 2, 0f, 0f);
+        }
     }
 }
