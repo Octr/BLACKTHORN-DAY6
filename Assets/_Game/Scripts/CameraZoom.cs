65a5a3dcc855b0730c822f146c03d6bb042c8bb6
Paris <paris@octr.dev>
2023-08-02 03:32:29 +0100

    CameraZoom Lock/Unlock functionality

--- Assets/_Game/Scripts/CameraZoom.cs
+++ Assets/_Game/Scripts/CameraZoom.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-public class CameraZoom : MonoBehaviour
+public class CameraZoom : Singleton<CameraZoom>
 {
+    public bool isUnlocked = false;
     public float zoomSpeed = 1.0f;
     public float minZoomSize = 2.0f;
     public float maxZoomSize = 10.0f;
@@ -11,7 +12,7 @@ public class CameraZoom : MonoBehaviour
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
         Camera mainCamera = Camera.main;
 
-        if (mainCamera != null)
+        if (mainCamera != null && isUnlocked)
         {
             // Adjust the camera's orthographic size based on the scroll input
             mainCamera.orthographicSize = Mathf.Clamp(mainCamera.orthographicSize - scrollInput * zoomSpeed, minZoomSize, maxZoomSize);
